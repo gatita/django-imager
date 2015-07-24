31657937394f87e2777a06b1c76764864d0bc0ce
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-24 13:46:39 -0700

    extend imager profile model

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
 
 
-# @python_2_unicode_compatible
+@python_2_unicode_compatible
 class ImagerProfile(models.Model):
     user = models.OneToOneField(
         User,
         related_name='profile',
         null=False
     )
-    camera = models.CharField()
+    camera = models.CharField(
+        max_length=255,
+        help_text='What is the make and model of your camera?'
+    )
     address = models.TextField()
     website = models.URLField()
-    photo_genre = models.CharField()
+    photo_genre = models.CharField(max_length=255)
 
     def is_active(self):
         pass

2a9ea36376e431f118fd19f3424847c9ae607fbd
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-24 16:59:09 -0700

    start tests for Photo

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from imager_images.models import Photo, Album
+import factory
+from faker import Faker
 
-# Create your tests here.
+
+fake = Faker()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    email = factory.Sequence(lambda n: 'user{}@example.com'.format(n))
+
+
+class PhotoFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Photo
+
+    user = 
+
+
+class PhotoTestCase(TestCase):
+
+    def setUp(self):
+        user = UserFactory.build()
+
+    def tearDown(self):
+        pass
+
+    def test_

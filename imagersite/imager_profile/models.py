3ed65ec733b4203605642d43c42795a3ac07fece
tlake <tanner.lake@gmail.com>
2015-07-28 14:43:12 -0700

    added utf-8 coding

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
 from django.contrib.auth.models import User
 
 

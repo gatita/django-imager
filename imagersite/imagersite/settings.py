d6939847596660511764d60c3c08e3d13abeff5d
tlake <tanner.lake@gmail.com>
2015-07-28 15:21:39 -0700

    imagersite added to INSTALLED_APPS for access to homepage

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imagersite',
     'imager_profile',
     'imager_images',
     'bootstrap3',

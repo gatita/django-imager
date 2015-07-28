6bab08c4dfdb178210e75a0bc5d2705487348906
tlake <tanner.lake@gmail.com>
2015-07-28 12:33:42 -0700

    importing unicode_literals

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
 from django.contrib.auth.models import User
 
 

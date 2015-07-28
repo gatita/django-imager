0b6acde841763d9b96219977c5efed9e40ec3118
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-28 13:32:13 -0700

    move receivers to handlers

---
+++ imagersite/imager_profile/handlers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from models import ImagerProfile
+
+
+@receiver(post_save, sender=User)
+def auto_create_profile(sender, instance, **kwargs):
+    try:
+        instance.profile
+    except ImagerProfile.DoesNotExist:
+        instance.profile = ImagerProfile()
+        instance.profile.save()
+
+
+@receiver(post_delete, sender=User)
+def auto_delete_profile(sender, instance, **kwargs):
+    try:
+        instance.profile.delete()
+    except ImagerProfile.DoesNotExist:
+        pass

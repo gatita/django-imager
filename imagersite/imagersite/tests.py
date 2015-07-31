d8b86cdf977e9e176f0e900ffbdae57de2804aba
tlake <tanner.lake@gmail.com>
2015-07-30 17:54:45 -0700

    registration tests

--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-# import html5lib
-# from django.core import mail
+from django.core import mail
 from django.contrib.auth.models import User
 import factory
 
@@ -52,3 +51,61 @@ class LoginLogoutTestCase(TestCase):
         soup = BeautifulSoup(response.content, 'html5lib')
         self.assertTrue(soup.find('a', text='Login'))
         self.assertFalse(soup.find('a', text='Logout'))
+
+
+class RegistrationTestCase(TestCase):
+    def setUp(self):
+        user = UserFactory()
+        user.set_password('longfallboots')
+        user.save()
+
+    def test_new_registration(self):
+        client = Client()
+        username = 'companion_cube'
+        email = 'do_not_incinerate@mailinator.com'
+        response = client.post(
+            '/accounts/register/',
+            {
+                'username': username,
+                'email': email,
+                'password1': 'iamsentient',
+                'password2': 'iamsentient',
+            },
+            follow=True,
+        )
+
+        user = User.objects.get(username=username)
+        self.assertFalse(user.is_active)
+
+        # check redirect page works
+        soup = BeautifulSoup(response.content, 'html5lib')
+        self.assertTrue(soup.find(
+            text="Please check your email to "
+            "complete the registration process."
+        ))
+
+        # check mail.outbox
+        domain = 'example.com'
+        link_lead = domain + '/accounts/activate/'
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn(domain, mail.outbox[0].body)
+        self.assertIn(link_lead, mail.outbox[0].body)
+        self.assertIn(email, mail.outbox[0].recipients())
+
+    def test_existing_registration(self):
+        client = Client()
+        user = User.objects.get(username='testsubject1')
+        response = client.post(
+            '/accounts/register/',
+            {
+                'username': user.username,
+                'email': user.email,
+                'password1': 'longfallboots',
+                'password2': 'longfallboots',
+            },
+            follow=True,
+        )
+        self.assertIn(
+            'A user with that username already exists',
+            response.content
+        )

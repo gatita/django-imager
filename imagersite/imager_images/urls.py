ef99dc4f7c8376685a45fd1738e5b8e0ff725226
tlake <tanner.lake@gmail.com>
2015-08-01 16:46:11 -0700

    named library route

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -3,6 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^library/', login_required(TemplateView.as_view(
-                                     template_name='library.html')))
+    url(
+        r'^library/',
+        login_required(
+            TemplateView.as_view(template_name='library.html')
+        ),
+        name='library'
+    )
 ]

9305c10579a9b3cd54c6b8f7b1cf43321d6e5f16
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-01 16:53:00 -0700

    add album view and template

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from views import AlbumView
 
 urlpatterns = [
     url(r'^library/', login_required(TemplateView.as_view(
-                                     template_name='library.html')))
+                                     template_name='library.html'))),
+    url(r'^album/(?P<pk>\d+)/$', login_required(
+        AlbumView.as_view()), name='album_detail'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.views.generic import DetailView, ListView
 from django.shortcuts import render
+from models import Photo, Album
 
-# Create your views here.
+
+class AlbumView(DetailView):
+    model = Album
+    template_name = 'album_detail.html'
+
+

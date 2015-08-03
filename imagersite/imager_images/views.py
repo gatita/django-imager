d1aaf7c05c938ec503a5e5e6431bbbe01f84ef5e
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-03 16:34:12 -0700

    raise forbidden if user is not album owner

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.views.generic import DetailView
+from django.core.exceptions import PermissionDenied
 from models import Photo, Album
 
 
 class AlbumView(DetailView):
     model = Album
     template_name = 'album_detail.html'
+
+    def get_object(self, **kwargs):
+        obj = super(AlbumView, self).get_object(**kwargs)
+        if obj.user != self.request.user:
+            raise PermissionDenied
+        else:
+            return obj
 
 
 class PhotoView(DetailView):

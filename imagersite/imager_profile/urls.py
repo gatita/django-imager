a6cff1a7aeb5bcf50bf686587aa4269e4e646aa8
tlake <tanner.lake@gmail.com>
2015-08-03 22:07:53 -0700

    wrote route for profile_edit_view

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,8 +1,22 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from imager_profile.views import ProfileView
+from imager_profile.views import ProfileView, profile_edit_view
 
 
 urlpatterns = [
-    url(r'^$', login_required(ProfileView.as_view()), name='profile'),
+    url(
+        r'^$',
+        login_required(
+            ProfileView.as_view()
+        ),
+        name='profile'
+    ),
+    url(
+        r'^edit/$',
+        login_required(
+            profile_edit_view,
+        ),
+        name='profile_edit'
+    )
+
 ]

ec44aabc79a4260bd0770e79dd7167d08d8518e6
tlake <tanner.lake@gmail.com>
2015-08-03 22:47:14 -0700

    wrote profile_edit_view

--- imagersite/imager_profile/forms.py
+++ imagersite/imager_profile/forms.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from .models import ImagerProfile
+
+
+class UserEditForm(ModelForm):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(ModelForm):
+    class Meta:
+        model = ImagerProfile
+        fields = ['website', 'address', 'camera', 'photo_genre']

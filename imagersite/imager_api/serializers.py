3499d7764eabd42bf4f43c4f50ef8a6cbc23827f
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-19 14:55:29 -0700

    fix imports

--- imagersite/imager_api/serializers.py
+++ imagersite/imager_api/serializers.py
@@ -2,12 +2,13 @@ from imager_images.models import Photo
 from rest_framework import serializers
 
 
-class PhotoSerializers(serializers.HyperlinkedModelSerializer):
+class PhotoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Photo
         fields = (
             'title',
             'description',
+            'img',
             'date_created',
             'date_modified',
             'date_published',

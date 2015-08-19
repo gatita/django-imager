9b1e80a42e525589ffd10647f36fca62299f6d0a
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-19 14:55:48 -0700

    add PhotoViewSet API endpoint

--- imagersite/imager_api/views.py
+++ imagersite/imager_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from imager_images.models import Photo
+from rest_framework import viewsets
+from serializers import PhotoSerializer
+from django.db.models import Q
 
-# Create your views here.
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to view  a list of their photos
+    """
+
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+    def get_queryset(self):
+        qset = super(PhotoViewSet, self).get_queryset(self)
+        public = Q(published='public')
+        if self.request.user.is_anonymous():
+            qset.objects.filter(public)
+
+        else:
+            private = Q(user=self.request.user)
+            qset.objects.filter(public | private).distinct()
